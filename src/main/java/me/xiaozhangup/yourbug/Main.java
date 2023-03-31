bbd5e15f2414558ee7805e0de71d7220df7cf865
Daniel Zhang <xiaozhangshiw@163.com>
2023-03-31 22:19:58 +0800

    Update README.md

--- src/main/java/me/xiaozhangup/yourbug/Main.java
+++ src/main/java/me/xiaozhangup/yourbug/Main.java
@@ -1,4 +1,6 @@
-package me.xiaozhangup.yourbug;public class Main {
+package me.xiaozhangup.yourbug;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
